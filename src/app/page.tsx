6ce80975446b9cf8a455fde95a943636d94ab409
Connor England <connor.england@fusionmgt.com>
2026-03-24 23:15:02 -0500

    fix: force dynamic rendering to prevent build-time prerender failure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Image from "next/image";
 import DashboardShell from "@/components/DashboardShell";
 import SeasonTracker from "@/components/SeasonTracker";
